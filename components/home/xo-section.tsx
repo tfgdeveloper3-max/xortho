e31f01d1bfda7f6dbc75c7c1ec89c2c67cf04087
tfgdeveloper3-max <tfgdeveloper3@gmail.com>
2026-02-21 03:03:43 +0500

    XO-section to full screen

--- components/home/xo-section.tsx
+++ components/home/xo-section.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image"
+import Image from "next/image";
+
 export default function XOSection() {
     return (
-        <section className="relative w-full md:min-h-screen h-[50vh] overflow-hidden bg-white flex items-start justify-center md:py-0">
+        <section className="relative w-full h-screen overflow-hidden bg-white flex items-center justify-center">
 
             {/* Background Video */}
             <div className="absolute inset-0 w-full h-full z-0 bg-[#2C2895]/50 flex items-center justify-center">
@@ -11,18 +12,18 @@ export default function XOSection() {
                     loop
                     muted
                     playsInline
-                    className="w-[300px] h-full object-cover mix-blend-darken"
+                    className="w-[200px] md:w-[300px] h-full object-cover mix-blend-darken"
                 />
             </div>
 
             {/* Overlay Image */}
-            <div className="absolute inset-0 w-full h-full bg-white mix-blend-screen z-10 flex items-start justify-center pointer-events-none select-none pt-20 md:pt-30">
+            <div className="absolute inset-0 w-full h-full bg-white mix-blend-screen z-10 flex items-center justify-center pointer-events-none select-none">
                 <Image
                     src="/images/XO.png"
                     alt="XO"
                     width={1000}
                     height={600}
-                    className="w-[80vw] md:w-[70vw] h-auto object-contain"
+                    className="w-[140%] h-[140%] sm:w-[130%] sm:h-[130%] md:w-[120%] md:h-[120%] object-contain"
                 />
             </div>
 
